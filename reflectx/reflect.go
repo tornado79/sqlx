ea5208dacd73b4a66f3f55f19e607d2d7d7eb6b9
tornado79 <72579025+tornado79@users.noreply.github.com>
2020-11-19 20:03:11 +0100

    Update reflect.go

--- reflectx/reflect.go
+++ reflectx/reflect.go
@@ -187,8 +187,14 @@ func (m *Mapper) TraversalsByNameFunc(t reflect.Type, names []string, fn func(in
 	mustBe(t, reflect.Struct)
 	tm := m.TypeMap(t)
 	for i, name := range names {
+
 		fi, ok := tm.Names[name]
 		if !ok {
+			 normName := strings.Replace(name, "_", "", -1)
+			 fi, ok = tm.Names[normName]
+		}
+		if !ok {
+
 			if err := fn(i, nil); err != nil {
 				return err
 			}
@@ -269,7 +275,9 @@ type typeQueue struct {
 // A copying append that creates a new slice each time.
 func apnd(is []int, i int) []int {
 	x := make([]int, len(is)+1)
-	copy(x, is)
+	for p, n := range is {
+		x[p] = n
+	}
 	x[len(x)-1] = i
 	return x
 }
